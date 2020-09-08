9765f8ba7523ee9e1592ea4d80d65fa20f814840
Anshu Dagar <anshu17singh@gmail.com>
2020-09-09 04:34:16 +0530

    added env variabled

--- setting/setting.stg.js
+++ setting/setting.stg.js
@@ -1,7 +1,20 @@
 const settings = {
   "config": {
     "ENV": process.env["NODE_ENV"],
-    "PORT": process.env["PORT"]
+    "PORT": process.env["PORT"],
+    "REDIS":{
+      "PORT":process.env["REDIS:PORT"],
+      "HOST":process.env["REDIS:HOST"],
+      "PASSWORD":process.env["REDIS:PASSWORD"]
+    },
+    "MONGO":{
+        "HOST":process.env["MONGO:HOST"],
+        "DB_NAME":process.env["MONGO:DB_NAME"],
+        "USER":process.env["MONGO:USER"],
+        "PASS":process.env["MONGO:PASS"],
+        "PORT":process.env["MONGO:PORT"],
+        "REPLICA_SET":process.env["MONGO:REPLICA_SET"]
+    }
   }
 };
 
