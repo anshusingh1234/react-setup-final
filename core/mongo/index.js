74e83be9391636a2e87b49d25ddbed6b29aca3d9
Anshu Dagar <anshu17singh@gmail.com>
2020-09-23 21:58:46 +0530

    users list api

--- core/mongo/index.js
+++ core/mongo/index.js
@@ -2,6 +2,7 @@ const {instance: MongoDBInstance} = require("./db");
 const feeds = require("./feeds");
 const comments = require("./comments");
 const reactions = require("./reactions");
+const users = require("./users");
 
 const mongo = {};
 
@@ -10,10 +11,12 @@ mongo.initMongoDB = async() => {
   await feeds.instance.init();
   await comments.instance.init();
   await reactions.instance.init();
+  await users.instance.init();
 }
 
 mongo.feeds = feeds;
 mongo.comments = comments;
 mongo.reactions = reactions;
+mongo.users = users;
 
 module.exports = mongo;
