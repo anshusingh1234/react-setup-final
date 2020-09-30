ab94ad487d786fc65feb92a1ef46639f477ce06f
Anshu Dagar <anshu17singh@gmail.com>
2020-09-30 17:37:30 +0530

    friend suggestion api

--- routes/v2/users/index.js
+++ routes/v2/users/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const userList = require("./list");
+const friends = require("./friends");
 const syncContacts = require("./syncContacts");
 const auth = require('../auth');
 const error = require('../error');
 
 router.get(`/list`,
 userList.list,
+error
+)
+
+router.get(`/friendSuggestions`,
+auth,
+friends.suggestions,
 error
 )
 
